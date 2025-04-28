daf7755c5577048ae8731653a8064cf80a5bf874
Iara Bongiovanni <bongiovanniiara22@gmail.com>
2025-04-28 18:53:48 -0300

    Finalización front

--- app/Views/front/principal.php
+++ app/Views/front/principal.php
@@ -1,33 +1,30 @@
 <!-- Carrusel de imágenes -->
 <div class="container-principal">
-    <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src=<?= base_url("assets/img/Presentacion/presentacion1.png") ?> class="d-block img-fluid mx-auto"
-                    alt="presentacion1">
-            </div>
-            <div class="carousel-item">
-                <img src=<?= base_url("assets/img/Presentacion/presentacion2.png") ?> class="d-block img-fluid mx-auto"
-                    alt="presentacion2">
-            </div>
-            <div class="carousel-item">
-                <img src=<?= base_url("assets/img/Presentacion/presentacion3.png") ?> class="d-block img-fluid mx-auto"
-                    alt="presentacion3">
-            </div>
-        </div>
-
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"
-            data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying"
-            data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
+  <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+    
+    <div class="carousel-inner">
+      <div class="carousel-item active" data-bs-interval="3000">
+        <img src="<?= base_url('assets/img/Presentacion/presentacion1.png') ?>" class="d-block w-100" alt="presentacion1">
+      </div>
+      <div class="carousel-item" data-bs-interval="3000">
+        <img src="<?= base_url('assets/img/Presentacion/presentacion2.png') ?>" class="d-block w-100" alt="presentacion2">
+      </div>
+      <div class="carousel-item" data-bs-interval="3000">
+        <img src="<?= base_url('assets/img/Presentacion/presentacion3.png') ?>" class="d-block w-100" alt="presentacion3">
+      </div>
     </div>
+
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+</div>
 
   <!-- Texto de bienvenida -->
 <div class="container-texto-bienvenida">
