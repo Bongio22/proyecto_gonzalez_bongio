4cbf15dd68a228308090f186ec972069bd7eae42
Ariel Gonzalez <arielgonzalezr9@gmail.com>
2025-05-24 19:11:49 -0300

    nuevo carrito

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,4 +1,5 @@
 <?php
+
 use CodeIgniter\Router\RouteCollection;
 
 /**
@@ -14,7 +15,7 @@ $routes->get('comercializacion', 'Home::comercializacion');
 $routes->get('terminosyUsos', 'Home::terminos_usos');
 /*Rutas principales*/
 
-/*Rutas par el manejo de Usuario*/ 
+/*Rutas par el manejo de Usuario*/
 $routes->get('registrarse', 'Home::registrarse'); // Ruta para mostrar el formulario
 $routes->post('registrarse', 'loginController::registrarUsuario');
 $routes->get('iniciarSesion', 'Home::iniciar_sesion');
@@ -23,23 +24,23 @@ $routes->get('cerrarSesion', 'loginController::cerrarSesion');
 $routes->get('misDatos', 'Home::modificarDatos');
 $routes->post('misDatos', 'loginController::modificarUsuario');
 $routes->get('panelAdmin', 'Home::panelAdmin');
-/*Rutas para el manejo de Usuario*/ 
+/*Rutas para el manejo de Usuario*/
 
-/*Rutas para el manejo de Productos*/ 
+/*Rutas para el manejo de Productos*/
 $routes->get('productos', 'productosController::productos'); // Ruta para listar productos
 $routes->get('productosAdmin', 'productosController::productosAdmin');
 $routes->get('modificarProducto/(:num)', 'productosController::cargarVistaModificarProducto/$1');
 $routes->post('modificarProducto', 'productosController::modificarProducto'); // Ruta para guardar cambios
 $routes->post('agregarProducto', 'productosController::guardarProducto');    // Ruta para guardar el producto
 $routes->get('productosController/eliminarProducto/(:num)', 'productosController::eliminarProducto/$1');
-/*Rutas para el manejo de Productos*/ 
+/*Rutas para el manejo de Productos*/
 
-/*Rutas para el manejo del Carrito*/ 
+/*Rutas para el manejo del Carrito*/
 $routes->get('agregarProducto', 'productosController::agregarProducto'); // Ruta para cargar el formulario
 
 $routes->get('carrito', 'CarritoController::carrito'); // Ruta para ver el carrito
-$routes->post('agregarAlCarrito/(:num)', 'CarritoController::add_carrito/$1');
+$routes->post('agregarAlCarrito/(:num)', 'carritoController::agregar/$1');
 $routes->get('agregarAlCarrito/(:num)', 'CarritoController::add_carrito/$1');
 $routes->post('eliminarDelCarrito', 'CarritoController::remover_producto');
 
-/*Rutas para el manejo del Carrito*/ 
+/*Rutas para el manejo del Carrito*/
